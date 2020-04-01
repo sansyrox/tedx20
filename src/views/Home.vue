32f9c0e71dd5e28e9c7db39f1bdce5844adcf711
Abhishek Aryan <22198661+Lunchb0ne@users.noreply.github.com>
2020-04-02 03:44:10 +0530

    Init Project (#3)
    Make the initial Skeleton

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template
+  ><div>
+    <section class="hero is-fullheight is-bold is-white round">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            TedXDTU
+          </h1>
+          <h2 class="subtitle">
+            Independently Organised Ted Event
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section id="sec1" class="section is-large">
+      <div class="container">
+        <h1 class="title">Section1</h1>
+        <h2 class="subtitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          eum necessitatibus similique amet deserunt totam nemo id porro laborum
+          velit, expedita fugit delectus recusandae tenetur corrupti deleniti
+          pariatur eos adipisci!
+        </h2>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+.round {
+  border-radius: 2rem 2rem 0px 0px;
+}
+#sec1 {
+  background-color: whitesmoke;
+}
+</style>
