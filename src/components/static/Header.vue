4fc4f2a85ec7cb184105789fa4c36092ed73daa9
pranjul139 <55094986+pranjul139@users.noreply.github.com>
2020-04-08 01:40:07 +0530

    Theme page and a new image folder (#5)
    * Updated About.vue
    
    * Theme Page and Added to new Image folder
    
    * Updated About.vue
    
    Tried resolving conflicts
    
    * Footer, Updated Header & index.js

--- src/components/static/Header.vue
+++ src/components/static/Header.vue
@@ -1,28 +1,37 @@
 <template>
   <b-navbar>
     <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <!-- <img
-          src="./TedX.png"
-          alt="TedXDTU"
-        /> -->
-      </b-navbar-item>
+      <div class="logo">
+        <img alt="TEDxDTU" src="images/logoWhite.png" />
+      </div>
     </template>
+
     <template slot="end">
-      <b-navbar-item href="#">
+      <b-navbar-item class="link" href="#">
         Home
       </b-navbar-item>
-      <b-navbar-dropdown hoverable label="Info">
-        <b-navbar-item href="#">
-          About
-        </b-navbar-item>
-        <b-navbar-item href="#">
-          Contact
-        </b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: 'SpeakerFrom' }">
-          Speaker
-        </b-navbar-item>
-      </b-navbar-dropdown>
+      <b-navbar-item class="link" href="#">
+        About
+      </b-navbar-item>
+      <b-navbar-item class="link" href="#">
+        Theme
+      </b-navbar-item>
+      <b-navbar-item class="link" href="#">
+        Speakers
+      </b-navbar-item>
+      <b-navbar-item class="link" href="#">
+        Past Events
+      </b-navbar-item>
+      <b-navbar-item class="link" href="#">
+        Partners
+      </b-navbar-item>
+      <b-navbar-item
+        class="link"
+        tag="router-link"
+        :to="{ path: 'SpeakerFrom' }"
+      >
+        Speaker Form
+      </b-navbar-item>
     </template>
 
     <template slot="end">
@@ -30,7 +39,7 @@
         <div class="buttons are-medium">
           <a class="button is-primary">
             <user-icon class="icon" />
-            <strong>Sign up</strong>
+            <strong>Register</strong>
           </a>
         </div>
       </b-navbar-item>
@@ -48,6 +57,38 @@ export default {
 </script>
 <style lang="scss" scoped>
 .navbar {
-  height: 70px;
+  height: 90px;
+  top: 0;
+  background: #111;
+  z-index: 10000;
+}
+.link {
+  color: #fff;
+  font-size: 0.9em;
+  font-weight: 600;
+  padding: 15px 25px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.075);
+  cursor: pointer;
+  -webkit-transition: all 0.5s ease;
+  -moz-transition: all 0.5s ease;
+  -ms-transition: all 0.5s ease;
+  transition: all 0.5s ease;
+}
+.link:hover {
+  color: #f3f9fb;
+  background: #ba0f12;
+}
+
+.logo {
+  position: absolute;
+  left: 5%;
+  height: 60px;
+  width: 150px;
+  top: 60%;
+  transform: translate(0, -50%);
+}
+img {
+  height: 60px;
+  width: 150px;
 }
 </style>
---
+++ src/views/Theme.vue
@@ -0,0 +1,97 @@
+<template>
+  <section
+    id="sec3"
+    class="theme"
+    style=" margin-top: 10px;background-image:url('https://uploads-ssl.webflow.com/5a9f02ce6e90d20001b22474/5aa18b573ee9510001161d18_slant_attend_top.svg'); background-size:cover; background-repeat:no-repeat;"
+  >
+    <div>
+      <h2 style="font-size:2.5rem;">Theme</h2>
+      <hr style="border:2px solid #c00; width:5rem; margin-top:20px;" />
+      <h1 style="color:rgb(0,0,0);margin-top:8%;">Virtue of Magnanimity</h1>
+    </div>
+    <div class="gallery-wrapper">
+      <p data-aos="fade-up" style="text-align:justify">
+        When faced with integral life choices, do you tend to pursue the choice
+        which is going to propel you to greatness and is beneficial for
+        everybody around you or do you fear failure and tend to stay away from
+        those choices that will push you out of your comfort zone, even though
+        they are the most beneficial and more honourable.
+      </p>
+      <hr style="border:1px solid #c00; width:2.5rem;" />
+      <p>
+        Magnanimity means “Nobility or Greatness of one’s soul”. It's known
+        basically as the virtue with which man pursues the choice that is most
+        difficult but is also the most honourable. Basically the stretching
+        forth of mind to achieve great and noble things also such that a person
+        achieves great things keeping his ability in check.
+      </p>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <iframe
+          style="width:80%; margin:auto; border:solid #c00 1.5px;"
+          height="400px"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Theme",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+.theme {
+  text-align: center;
+  padding: 40px 0px;
+}
+
+.gallery-wrapper {
+  width: 90%;
+  margin: auto;
+  padding: 15px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+p {
+  font-family: "Quicksand", sans-serif;
+  line-height: normal;
+  font-size: 1.1em;
+}
+
+h1 {
+  width: 80%;
+  margin: auto;
+  padding: 40px 0px;
+  font-family: "EB Garamond", sans-serif;
+  font-size: 4.5em;
+  font-weight: 700;
+  color: #fff;
+  letter-spacing: 10px;
+  text-shadow: 10px 10px 40px rgba(0, 0, 0, 0.3);
+  text-transform: uppercase;
+  text-align: center;
+}
+h2 {
+  width: 80%;
+  margin: auto;
+  font-family: "EB Garamond", serif;
+  font-weight: 700;
+  color: #000000;
+  letter-spacing: 2.5px;
+  text-transform: uppercase;
+  text-align: center;
+}
+iframe {
+  align-self: justify;
+}
+</style>
